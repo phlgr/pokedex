d6c5e65ea9a5fe2d2e7e60d2ef41ffb9d7b24ad3
phlgr <philippm.gartz@gmail.com>
2020-03-02 16:11:52 +0100

    Fix test script

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,24 @@ module.exports = {
   devServer: {
     contentBase: './dist'
   },
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ]
+      }
+    ]
   }
 };
