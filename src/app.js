fde6cdc8ed29944077728d4120f1d3dde38d781f
phlgr <philippm.gartz@gmail.com>
2020-03-03 13:57:36 +0100

    Add search field and database

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import './app.scss';
 import { createElement } from './lib/dom';
 import { createSearch } from './components/search';
 import { createTitle } from './components/title';
+import { pokemon } from './components/pokemon';
 
 export function app() {
   const header = createElement('header', {
@@ -10,14 +11,20 @@ export function app() {
   const main = createElement('main', {
     className: 'main'
   });
+  const cardText = createElement('div', {
+    className: 'main__text'
+  });
+
   const title = createTitle('Pokedex');
   const search = createSearch();
 
   header.appendChild(title);
   header.appendChild(search);
 
+  main.appendChild(cardText);
+
   search.addEventListener('input', searchField => {
-    console.log(searchField.target.value);
+    cardText.innerText = searchField.target.value;
   });
 
   return [header, main];
